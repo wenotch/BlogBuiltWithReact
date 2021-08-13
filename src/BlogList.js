681b9b298bef3a84900bc482894521b18d33dec0
Nwanochie Emmanuel <emmanuel.nwanochie.197850@unn.edu.ng>
2021-08-13 19:24:45 +0100

    Created the home

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 function BlogList(props) {
-    return (
-      <div className="blog-preview" key={props.id}>
-        <h2>{props.title}</h2>
-        <p>written by {props.author}</p>
-      </div>
-    );
+  return (
+    <div className="blog-preview" key={props.id}>
+      <h2>{props.title}</h2>
+      <p>written by {props.author}</p>
+      <button onClick={props.delete}>Delete</button>
+    </div>
+  );
 }
 
-export default BlogList
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -3,14 +3,25 @@ import BlogList from "./BlogList";
 
 function Home() {
   const [blogs, setblogs] = useState([
-    { title: "it is", body: "body", aurthor: "nwanoch", id: 1 },
-    { title: "it is", body: "body", aurthor: "nwanoch", id: 2 },
-    { title: "it is", body: "body", aurthor: "nwanoch", id: 3 },
+    { title: "it is 1", body: "body", aurthor: "nwanoch", id: 1 },
+    { title: "it is 2", body: "body", aurthor: "nwanoch", id: 2 },
+    { title: "it is 3", body: "body", aurthor: "nwanoch", id: 3 },
   ]);
+
+  const handleDelete = (id) => {
+    const newblog = blogs.filter((blog) => blog.id !== id);
+    setblogs(newblog);
+  };
   return (
     <div className="home">
+      <h2>All blogs</h2>
       {blogs.map((blog) => (
-        <BlogList title={blog.title} author={blog.title} id={blog.id}/>
+        <BlogList
+          title={blog.title}
+          author={blog.title}
+          id={blog.id}
+          delete={() => handleDelete(blog.id)}
+        />
       ))}
     </div>
   );
